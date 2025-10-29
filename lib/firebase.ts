faf9f966c936d0c2d20cd9b34ad6526dc8e117e5
cowoksoftspoken <dbgaming679@gmail.com>
2025-10-29 23:19:39 +0700

    .

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,7 +5,8 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAOkE27nXxkipOczHekDsaoS3tXpqWOIEo",
-  authDomain: "react-chat-98ca7.firebaseapp.com",
+  // authDomain: "react-chat-98ca7.firebaseapp.com",
+  authDomain: "auth.telepaty.my.id",
   databaseURL: "https://react-chat-98ca7-default-rtdb.firebaseio.com",
   projectId: "react-chat-98ca7",
   storageBucket: "react-chat-98ca7.appspot.com",
